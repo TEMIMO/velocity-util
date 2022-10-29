49a99051d469cf6ed9887be3fe1a4aec153681f9
aivanov <a.ivanov@bftcom.com>
2022-10-29 18:37:33 +0300

    Commit

--- src/main/java/VelocityConverter.java
+++ src/main/java/VelocityConverter.java
@@ -8,6 +8,7 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -15,16 +16,17 @@ public class VelocityConverter {
     public static void main(String[] args) {
         VelocityContext context = new VelocityContext();
 
-        File file = new File("src/main/resources/ПФ_103ПП.xml");
+        File file = new File("src/main/resources/ПВ-Израиль_банки.xml");
         XStream xStream = new XStream(new DomDriver());
-        xStream.alias("payreg", Map.class);
+        // Написать корневой элемент вместо mass
+        xStream.alias("mass", Map.class);
         xStream.registerConverter(new MapEntryConverter());
         Map<String,Object> map = (Map<String,Object>) xStream.fromXML(file);
 
         context.put("data", map);
 
         StringWriter stringWriter = new StringWriter();
-        Reader reader = new StringReader("src/main/resources/example");
+        Reader reader = new StringReader("src/main/resources/sheet1.xml");
         Velocity.evaluate(context, stringWriter, "VelocityService", reader);
 
         byte[] bytes = stringWriter.toString().getBytes(Charset.forName("windows-1251"));
