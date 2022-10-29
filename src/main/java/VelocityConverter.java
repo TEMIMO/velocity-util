781585ec49d17eaa95b788093264a228b5e1f553
aivanov <a.ivanov@bftcom.com>
2022-10-29 19:14:47 +0300

    Commit

--- src/main/java/VelocityConverter.java
+++ src/main/java/VelocityConverter.java
@@ -1,19 +1,17 @@
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
+import org.apache.commons.io.FileUtils;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
 
-import java.io.File;
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Map;
 
 public class VelocityConverter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         VelocityContext context = new VelocityContext();
 
         File file = new File("src/main/resources/ПВ-Израиль_банки.xml");
@@ -26,11 +24,13 @@ public class VelocityConverter {
         context.put("data", map);
 
         StringWriter stringWriter = new StringWriter();
-        Reader reader = new StringReader("src/main/resources/sheet1.xml");
+        Reader reader = new FileReader("src/main/resources/sheet1.xml");
         Velocity.evaluate(context, stringWriter, "VelocityService", reader);
 
         byte[] bytes = stringWriter.toString().getBytes(Charset.forName("windows-1251"));
         String result = Arrays.toString(bytes);
+
+        FileUtils.writeByteArrayToFile(new File("src/main/resources/result.txt"), bytes);
 
         System.out.println(result);
     }
