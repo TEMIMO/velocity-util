9452e90725957d50363d22020bdcf8d12ef6d0c4
aivanov <a.ivanov@bftcom.com>
2022-10-31 14:40:14 +0300

    Commit

--- src/main/java/VelocityConverter.java
+++ src/main/java/VelocityConverter.java
@@ -3,6 +3,8 @@ import com.thoughtworks.xstream.io.xml.DomDriver;
 import org.apache.commons.io.FileUtils;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
+import ru.gov.pfr.ecp.fo.printedform.lib.util.DateConverter;
+import ru.gov.pfr.ecp.fo.printedform.lib.util.NumbersTool;
 
 import java.io.*;
 import java.nio.charset.Charset;
@@ -14,14 +16,16 @@ public class VelocityConverter {
     public static void main(String[] args) throws IOException {
         VelocityContext context = new VelocityContext();
 
-        File file = new File("src/main/resources/in/ПВ-Израиль_банки.xml");
+        File file = new File("src/main/resources/in/ПФ_103ПП.xml");
         XStream xStream = new XStream(new DomDriver());
         // Написать корневой элемент вместо mass
-        xStream.alias("mass", Map.class);
+        xStream.alias("payreg", Map.class);
         xStream.registerConverter(new MapEntryConverter());
         Map<String,Object> map = (Map<String,Object>) xStream.fromXML(file);
 
         context.put("data", map);
+        context.put("numbersTool", new NumbersTool());
+        context.put("dateConverter", new DateConverter());
 
         StringWriter stringWriter = new StringWriter();
         Reader reader = new FileReader("src/main/resources/in/sheet1.xml", StandardCharsets.UTF_8);
