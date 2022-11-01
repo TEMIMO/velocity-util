5332a0bb1c0dc0f6a022abbde92b2d51c97ca499
aivanov <a.ivanov@bftcom.com>
2022-11-01 12:38:56 +0300

    Commit

--- src/main/java/MapEntryConverter.java
+++ src/main/java/MapEntryConverter.java
@@ -4,9 +4,7 @@ import com.thoughtworks.xstream.converters.UnmarshallingContext;
 import com.thoughtworks.xstream.io.HierarchicalStreamReader;
 import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 
-import java.util.AbstractMap;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MapEntryConverter implements Converter {
 
@@ -31,7 +29,7 @@ public class MapEntryConverter implements Converter {
 
     public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
 
-        Map<String, String> map = new HashMap<String, String>();
+        Map<String, Object> map = new HashMap<String, Object>();
         Map <String, Integer> numbersMap = new HashMap<String, Integer>();
 
 
@@ -42,97 +40,8 @@ public class MapEntryConverter implements Converter {
             String key = reader.getNodeName();
             String value = reader.getValue();
 
-            while(reader.hasMoreChildren()) {
-
-                reader.moveDown();
-
-                String key1 = key + reader.getNodeName();
-                String value1 = reader.getValue();
-
-                while(reader.hasMoreChildren()) {
-
-                    reader.moveDown();
-
-                    String key2 = key1 + reader.getNodeName();
-                    String value2 = reader.getValue();
-
-                    while(reader.hasMoreChildren()) {
-
-                        reader.moveDown();
-
-                        String key3 = key2 + reader.getNodeName();
-                        String value3 = reader.getValue();
-
-                        while(reader.hasMoreChildren()) {
-
-                            reader.moveDown();
-
-                            String key4 = key3 + reader.getNodeName();
-                            String value4 = reader.getValue();
-
-                            if (!numbersMap.containsKey(key4)) {
-                                numbersMap.put(key4, 1);
-                                key4 += numbersMap.get(key4);
-                            }
-
-                            if (map.containsKey(key4)) {
-                                numbersMap.put(key4, numbersMap.get(key4) + 1);
-                                key4 += numbersMap.get(key4);
-                            }
-
-                            map.put(key4, value4);
-
-                            reader.moveUp();
-                        }
-
-                        if (!numbersMap.containsKey(key3)) {
-                            numbersMap.put(key3, 1);
-                            key3 += numbersMap.get(key3);
-                        }
-
-                        if (map.containsKey(key3)) {
-                            numbersMap.put(key3, numbersMap.get(key3) + 1);
-                            key3 += numbersMap.get(key3);
-                        }
-
-                        map.put(key3, value3);
-
-                        reader.moveUp();
-                    }
-
-                    if (!numbersMap.containsKey(key2)) {
-                        numbersMap.put(key2, 1);
-                        key2 += numbersMap.get(key2);
-                    }
-
-                    if (map.containsKey(key2)) {
-                        numbersMap.put(key2, numbersMap.get(key2) + 1);
-                        key2 += numbersMap.get(key2);
-                    }
-
-                    map.put(key2, value2);
-
-                    reader.moveUp();
-                }
-
-                if (!numbersMap.containsKey(key1)) {
-                    numbersMap.put(key1, 1);
-                    key1 += numbersMap.get(key1);
-                }
-
-                if (map.containsKey(key1)) {
-                    numbersMap.put(key1, numbersMap.get(key1) + 1);
-                    key1 += numbersMap.get(key1);
-                }
-
-                map.put(key1, value1);
-
-                reader.moveUp();
-            }
-
             if (!numbersMap.containsKey(key)) {
-                numbersMap.put(key, 1);
-                key += numbersMap.get(key);
+                numbersMap.put(key, 0);
             }
 
             if (map.containsKey(key)) {
@@ -141,6 +50,98 @@ public class MapEntryConverter implements Converter {
             }
 
             map.put(key, value);
+            Map<String, Object> map1 = new HashMap<>();
+
+            while(reader.hasMoreChildren()) {
+
+                reader.moveDown();
+
+                String key1 = reader.getNodeName();
+                String value1 = reader.getValue();
+
+                if (!numbersMap.containsKey(key1)) {
+                    numbersMap.put(key1, 0);
+                }
+
+                if (map1.containsKey(key1)) {
+                    numbersMap.put(key1, numbersMap.get(key1) + 1);
+                    key1 += numbersMap.get(key1);
+                }
+
+                map1.put(key1, value1);
+                map.put(key, map1);
+                Map<String, Object> map2 = new HashMap<>();
+
+                while(reader.hasMoreChildren()) {
+
+                    reader.moveDown();
+
+                    String key2 = reader.getNodeName();
+                    String value2 = reader.getValue();
+
+                    if (!numbersMap.containsKey(key2)) {
+                        numbersMap.put(key2, 0);
+                    }
+
+                    if (map2.containsKey(key2)) {
+                        numbersMap.put(key2, numbersMap.get(key2) + 1);
+                        key2 += numbersMap.get(key2);
+                    }
+
+                    map2.put(key2, value2);
+                    map1.put(key1, map2);
+                    Map<String, Object> map3 = new HashMap<>();
+
+                    while(reader.hasMoreChildren()) {
+
+                        reader.moveDown();
+
+                        String key3 = reader.getNodeName();
+                        String value3 = reader.getValue();
+
+                        if (!numbersMap.containsKey(key3)) {
+                            numbersMap.put(key3, 0);
+                        }
+
+                        if (map3.containsKey(key3)) {
+                            numbersMap.put(key3, numbersMap.get(key3) + 1);
+                            key3 += numbersMap.get(key3);
+                        }
+
+                        map3.put(key3, value3);
+                        map2.put(key2, map3);
+                        Map<String, Object> map4 = new HashMap<>();
+
+                        while(reader.hasMoreChildren()) {
+
+                            reader.moveDown();
+
+                            String key4 = reader.getNodeName();
+                            String value4 = reader.getValue();
+
+                            if (!numbersMap.containsKey(key4)) {
+                                numbersMap.put(key4, 0);
+                            }
+
+                            if (map4.containsKey(key4)) {
+                                numbersMap.put(key4, numbersMap.get(key4) + 1);
+                                key4 += numbersMap.get(key4);
+                            }
+
+                            map4.put(key4, value4);
+                            map3.put(key3, map4);
+
+                            reader.moveUp();
+                        }
+
+                        reader.moveUp();
+                    }
+
+                    reader.moveUp();
+                }
+
+                reader.moveUp();
+            }
 
             reader.moveUp();
         }
